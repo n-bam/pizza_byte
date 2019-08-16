4976e1a40f628a0b7a39241ab6ed34cf6ef62926
Huxley Ruanis <Huxxley@hotmail.com.br>
2019-08-16 16:03:14 -0300

    Iniciado a incrementação do backup do sistema.

--- PizzaByteSite/Controllers/BackupController.cs
+++ PizzaByteSite/Controllers/BackupController.cs
@@ -1,4 +1,5 @@
-﻿using PizzaByteBll.Base;
+﻿using PizzaByteBll;
+using PizzaByteBll.Base;
 using PizzaByteDto.Base;
 using PizzaByteDto.RetornosRequisicoes;
 using PizzaByteSite.Models;
@@ -8,10 +9,10 @@ using System.Web.Script.Serialization;
 
 namespace PizzaByteSite.Controllers
 {
-    public class BackupController : BaseController
+    public class BackupController : Controller
     {
         /// <summary>
-        /// Chama a tela com a listagem de Backup
+        /// Chama a tela com a listagem de mensagens de sackup
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
@@ -22,62 +23,101 @@ namespace PizzaByteSite.Controllers
                 return RedirectToAction("Login", "Usuario");
             }
 
-            // Model a ser utilizada na tela
-            FiltrosBackupModel model = new FiltrosBackupModel()
-            {
-                Pagina = 1,
-                DataFinal = DateTime.Now,
-                DataInicial = DateTime.Now
-            };
-
-            string mensagemErro = "";
-            if (!Utilidades.PreencherListasFiltrosBackup(model, ref mensagemErro))
-            {
-                ViewBag.MensagemErro = mensagemErro;
-                return View("Erro");
-            }
-
             //Chamar a view
-            return View(model);
+            return View();
         }
 
         /// <summary>
-        /// Obtem uma listra filtrada de logs
+        /// Obtem uma listra filtrada de mensagens
         /// </summary>
         /// <param name="filtros"></param>
         /// <returns></returns>
-        public string ObterListaFiltrada(FiltrosBackupModel filtros)
+        public string ObterLista(BaseFiltrosModel filtros)
         {
             //Requisição para obter a lista
             RequisicaoObterListaDto requisicaoDto = new RequisicaoObterListaDto()
             {
-                CampoOrdem = "DATAINCLUSAO",
+                CampoOrdem = "DATADECRESCENTE",
                 IdUsuario = SessaoUsuario.SessaoLogin.IdUsuario,
                 Identificacao = SessaoUsuario.SessaoLogin.Identificacao,
-                NaoPaginarPesquisa = filtros.NaoPaginaPesquisa,
+                NaoPaginarPesquisa = false,
                 Pagina = filtros.Pagina,
-                NumeroItensPorPagina = 10
+                NumeroItensPorPagina = 5
             };
 
-            requisicaoDto.ListaFiltros.Add("DATAINCLUSAOINICIAL", filtros.DataInicial.ToString());
-            requisicaoDto.ListaFiltros.Add("DATAINCLUSAOFINAL", filtros.DataFinal.ToString());
+            //Consumir o serviço
+            BackupBll sackupBll = new BackupBll(true);
+            RetornoObterListaDto<BackupDto> retornoDto = new RetornoObterListaDto<BackupDto>();
+            sackupBll.ObterListaFiltrada(requisicaoDto, ref retornoDto);
 
-            //Adicionar filtros utilizados
-            if (!string.IsNullOrWhiteSpace(filtros.Mensagem))
+            string retorno = new JavaScriptSerializer().Serialize(retornoDto);
+            return retorno;
+        }
+
+        /// <summary>
+        /// Inclui uma mensagem de sackup
+        /// </summary>
+        /// <param name="filtros"></param>
+        /// <returns></returns>
+        public string IncluirMensagemBackup(BackupDto sackupDto)
+        {
+            RetornoDto retornoDto = new RetornoDto();
+            if (sackupDto == null)
             {
-                requisicaoDto.ListaFiltros.Add("MENSAGEM", filtros.Mensagem.Trim());
+                retornoDto.Mensagem = "Não foram recebidos os dados da mensagem";
+                retornoDto.Retorno = false;
             }
 
-            if (!string.IsNullOrWhiteSpace(filtros.IdUsuario))
-            {
-                requisicaoDto.ListaFiltros.Add("IDUSUARIO", filtros.IdUsuario.ToString());
-            }
+            sackupDto.Id = Guid.NewGuid();
 
+            //Requisição para incluir a mensagem
+            RequisicaoEntidadeDto<BackupDto> requisicaoDto = new RequisicaoEntidadeDto<BackupDto>()
+            {
+                IdUsuario = SessaoUsuario.SessaoLogin.IdUsuario,
+                Identificacao = SessaoUsuario.SessaoLogin.Identificacao,
+                EntidadeDto = sackupDto
+            };
 
             //Consumir o serviço
-            BackupBll logBll = new BackupBll("LogController");
-            RetornoObterListaDto<BackupDto> retornoDto = new RetornoObterListaDto<BackupDto>();
-            BackupBll.ObterListaFiltrada(requisicaoDto, ref retornoDto);
+            BackupBll sackupBll = new BackupBll(true);
+            sackupBll.Incluir(requisicaoDto, ref retornoDto);
+
+            RetornoGuidDto retornoGuidDto = new RetornoGuidDto()
+            {
+                Id = sackupDto.Id,
+                Retorno = retornoDto.Retorno,
+                Mensagem = retornoDto.Mensagem
+            };
+
+            string retorno = new JavaScriptSerializer().Serialize(retornoGuidDto);
+            return retorno;
+        }
+
+        /// <summary>
+        /// Exclui uma mensagem de sackup
+        /// </summary>
+        /// <param name="filtros"></param>
+        /// <returns></returns>
+        public string ExcluirMensagem(Guid idMensagem)
+        {
+            RetornoDto retornoDto = new RetornoDto();
+            if (idMensagem == Guid.Empty)
+            {
+                retornoDto.Mensagem = "O id da mensagem não foi informado.";
+                retornoDto.Retorno = false;
+            }
+
+            //Requisição para excluir
+            RequisicaoObterDto requisicaoDto = new RequisicaoObterDto()
+            {
+                IdUsuario = SessaoUsuario.SessaoLogin.IdUsuario,
+                Identificacao = SessaoUsuario.SessaoLogin.Identificacao,
+                Id = idMensagem
+            };
+
+            //Consumir o serviço
+            BackupBll sackupBll = new BackupBll(true);
+            sackupBll.Excluir(requisicaoDto, ref retornoDto);
 
             string retorno = new JavaScriptSerializer().Serialize(retornoDto);
             return retorno;
