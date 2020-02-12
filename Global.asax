31043cedfa7e192b753945355ee94c09ab8d2ea0
Nathália Cristina Nascimento <jlmanfrinato@hotmail.com>
2020-02-12 09:32:56 -0300

    Atualização dos arquivos do TFS
    Atualização dos arquivos do TFS

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PizzaByteSite.MvcApplication" Language="C#" %>
---
+++ SessaoUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web;
+
+namespace PizzaByteSite
+{
+    public class SessaoUsuario
+    {
+        public SessaoUsuario()
+        {
+            this.Identificacao = "";
+            this.NomeUsuario = "Não logado";
+            this.IdUsuario = Guid.Empty;
+            this.Administrador = false;
+        }
+
+        // Propriedades da sessão
+        public string NomeUsuario { get; set; }
+        public Guid IdUsuario { get; set; }
+        public string Identificacao { get; set; }
+        public bool Administrador { get; set; }
+
+        /// <summary>
+        /// Prepra a sessão
+        /// </summary>
+        public static SessaoUsuario SessaoLogin
+        {
+            get
+            {
+                SessaoUsuario sessao = (SessaoUsuario)HttpContext.Current.Session["_Sessao"];
+                if (sessao == null)
+                {
+                    sessao = new SessaoUsuario();
+                    HttpContext.Current.Session["_Sessao"] = sessao;
+                }
+
+                return sessao;
+            }
+        }
+    }
+}
